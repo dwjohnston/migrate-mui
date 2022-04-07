a48eb4c2e00b3b5425ba4dd76440410881cbf5b6
David Johnston <djohnston@cydarm.com>
2022-04-07 12:36:35 +1000

    Fix example

--- src/MyComponent2.tsx
+++ src/MyComponent2.tsx
@@ -1,20 +1,11 @@
 
 import React from 'react';
-import makeStyles from '@mui/styles/makeStyles';
-
+import { useStyles } from './MyComponent.styles';
 
 export type MyComponentProps = {
 };
 
 
-const useStyles = makeStyles(
-  ({ spacing, palette }) => ({
-    root: {
-        padding: spacing(2),
-        border: "solid 1px red",
-    }
-  })
-);
 
 
 
--- src/styles.tsx
+++ src/styles.tsx
@@ -1,4 +1,4 @@
-
+import React from "react"; 
 import makeStyles from '@mui/styles/makeStyles';
 
 export const useStyles = makeStyles(
