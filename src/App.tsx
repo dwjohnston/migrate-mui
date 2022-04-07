3de96cc466317f8c157736097350491d43493558
David Johnston <djohnston@cydarm.com>
2022-04-07 11:37:05 +1000

    Ran migrate

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,28 @@ import React from 'react';
 
 import './App.css';
 
-import { createTheme, makeStyles, ThemeProvider } from '@material-ui/core/styles';
+import { createTheme, ThemeProvider, Theme, StyledEngineProvider, adaptV4Theme } from '@mui/material/styles';
+import makeStyles from '@mui/styles/makeStyles';
 import { MyComponent } from './MyComponent';
 
-const theme = createTheme({});
+
+declare module '@mui/styles/defaultTheme' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  interface DefaultTheme extends Theme {}
+}
+
+
+const theme = createTheme(adaptV4Theme({}));
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <div className="App">
-        <MyComponent />
-      </div>
-    </ThemeProvider>
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <MyComponent />
+        </div>
+      </ThemeProvider>
+    </StyledEngineProvider>
   );
 }
 
